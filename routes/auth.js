190a2494cd492fdc5a41c16e8e217139c72d4237
Kunal Gautam <48962753+Kunnu1371@users.noreply.github.com>
2020-10-07 23:57:57 +0530

    modified: user route

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const  {signup, signin, signout, requireSignin}  = require('../controllers/auth')
 const {userSignupValidator} = require('../validator')
-router.post('/signup', userSignupValidator, signup);
-router.post('/signin', signin);
-router.get('/signout', signout);
+router.post('/user/signup', userSignupValidator, signup);
+router.post('/user/signin', signin);
+router.get('/user/signout', signout);
 
 router.get('/hello', requireSignin,  (req, res) => {
     res.send("Hello there")
